e17eb2a393d704d70b8a233ba87aae6ee73dc51e
Sgordon4 <SeanGordonkh@gmail.com>
2024-02-11 12:18:33 -0600

    Got Postgresql private server to work :)

--- database/postgresPool.js
+++ database/postgresPool.js
@@ -1,18 +1,21 @@
 const path = require('path');
 const {Pool} = require('pg');
 
+
 require('dotenv').config({
   override: true,
   path: path.join(__dirname, 'pgconfig.env')
 });
 
-//Grab pool properties from pgconfig.env
+
+//Grab pool properties from pgconfig.env 
 const pool = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.PORT
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
 });
+
 
 exports.POOL = pool
--- routes/postgres.js
+++ routes/postgres.js
@@ -4,18 +4,17 @@ var router = express.Router();
 const {POOL} = require("../database/postgresPool")
 
 
-
-
 router.get('/', function(req, res, next) {
-    res.send('Postgres output -');
+    // res.send('Postgres output -');
 
     (async () => {
-        const client = await pool.connect();
+        const client = await POOL.connect();
   
         try {
-            const {rows} = await client.query('SELECT * FROM account');
+            const {rows} = await client.query('SELECT * FROM account;');
             console.log("ROWS: ");
             console.log(rows);
+            res.send(rows);
         } 
         catch (err) {
             console.error(err);
