1f8b765d913aeb1ed8a66d49cdc7755cc7499948
Sgordon4 <SeanGordonkh@gmail.com>
2024-02-11 12:39:39 -0600

    Routing edits

--- routes/postgres.js
+++ routes/postgres.js
@@ -4,15 +4,57 @@ var router = express.Router();
 const {POOL} = require("../database/postgresPool")
 
 
-router.get('/', function(req, res, next) {
-    // res.send('Postgres output -');
 
+router.get('/', function(req, res, next) {
+    res.send('<h2>Postgres output -</h2>'+
+    'Try /accounts, /files, or /metadata');
+});
+
+router.get('/accounts', function(req, res, next) {
     (async () => {
         const client = await POOL.connect();
   
         try {
             const {rows} = await client.query('SELECT * FROM account;');
-            console.log("ROWS: ");
+            console.log("ACCOUNTS: ");
+            console.log(rows);
+            res.send(rows);
+        } 
+        catch (err) {
+            console.error(err);
+        } finally {
+            client.release();
+        }
+    })();
+});
+
+
+router.get('/files', function(req, res, next) {
+    (async () => {
+        const client = await POOL.connect();
+  
+        try {
+            const {rows} = await client.query('SELECT * FROM file;');
+            console.log("FILES: ");
+            console.log(rows);
+            res.send(rows);
+        } 
+        catch (err) {
+            console.error(err);
+        } finally {
+            client.release();
+        }
+    })();
+});
+
+
+router.get('/metadata', function(req, res, next) {
+    (async () => {
+        const client = await POOL.connect();
+  
+        try {
+            const {rows} = await client.query('SELECT * FROM metadata;');
+            console.log("METADATA: ");
             console.log(rows);
             res.send(rows);
         } 
