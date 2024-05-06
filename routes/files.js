61c0ff8e28dc6bc4e23e61ac05080069185e4673
Sgordon4 <SeanGordonkh@gmail.com>
2024-05-06 14:21:22 -0500

    Runnin out of steam

--- routes/files.js
+++ routes/files.js
@@ -35,60 +35,40 @@ Each file object includes (fileuid, filename, isdirectory, issymboliclink, creat
 */
 
 
+
+async function updateStateTableDate(fileUID, column) {
+	//Update, don't make a new entry
+	var updateStateSql = `update state set `
+		+`fileuid = '${fileUID}', `
+		+`${column} = (now() at time zone 'utc') `
+		+`where fileuid = '${fileUID}';`;
+
+	const client = await POOL.connect();
+	console.log(`Updating state ${column} with sql:\n${updateStateSql}`);
+	await client.query(updateStateSql);
+}
+
+
 //-----------------------------------------------------------------------------
 // Get Requests
 //-----------------------------------------------------------------------------
 
-//TODO Maybe require a body param with accountid or something? Sending only files for that account?
-router.get('/', function(req, res, next) {
-	console.log(`\nAttempting to fetch all files`);
-
-	const query = req.query;
-	console.log(`Available queries:`);
-	console.log(query);
-
-	//Grab any conditions we care about from the parameters sent with the request 
-	var conditions = [];
-	if(query.accountuid !== undefined) conditions.push("accountuid = '"+query.accountuid+"'");
-	if(query.parentuid !== undefined) conditions.push("parentuid = '"+query.parentuid+"'");
-	if(query.fileuid !== undefined) conditions.push("fileuid = '"+query.fileuid+"'");
-	console.log(`Accepted conditions:`);
-	console.log(conditions);
-
-
-	//Combine the conditions into a usable where query
-	const where = conditions.length > 0 ? " WHERE "+conditions.join(" AND ") : "";
-
-
-	var sql  = "SELECT fileuid, filename, isdirectory, issymboliclink, "
-			+"accountuid, parentuid, creationdate, deleted FROM file";
-	sql += where;
-	sql += " LIMIT 500;";
-	
-
-	(async () => {
-	  const client = await POOL.connect();
-
-		try {
-			console.log(`Selecting files with sql:\n${sql}`);
-			const {rows} = await client.query(sql);
-
-			res.send(rows);
-		} 
-		catch (err) {
-			console.error(err);
-			res.send(err);
-		} finally {
-			client.release();
-		}
-	})();
-});
-
-
-
 router.get('/:id', async function(req, res, next) {
 	const fileUID = req.params.id;
 	console.log(`\nAttempting to read file with UID='${fileUID}'`);
+
+
+	try {
+		updateStateTableDate(fileUID, "lastfileaccessdate");
+	} 
+	catch (err) {
+		console.error(err);
+		res.send(err);
+		return;
+	} finally {
+		client.release();
+	}
+	
 
 	console.log(`Generating signed get url...`);
 	IBMCOS.getSignedUrlPromise('getObject', { 
@@ -103,14 +83,20 @@ router.get('/:id', async function(req, res, next) {
 	});
 });
 
+//-----------------------------------------------------------------------------
+
 router.get('/properties/:id', async function(req, res, next) {
 	const fileUID = req.params.id;
 	console.log(`\nAttempting to fetch file properties with UID='${fileUID}'`);
 
-	var sql ="SELECT fileuid, filename, parentuid, accountuid, isdirectory, issymboliclink, "
-			+"userdefinedattr, tags, creationdate, deleted FROM file "
-			+"WHERE fileuid = '"+fileUID+"';";
 
+	updateStateTableDate(fileUID, "lastdbaccessdate", res);
+
+
+	var sql ="SELECT fileuid, filename, parentuid, accountuid, isdirectory, issymboliclink, "
+			+"userdefinedattr, tags, creationdate FROM file "
+			+"WHERE fileuid = '"+fileUID+"' "
+			+"AND deleted != true;";
 
 	(async () => {
 	  const client = await POOL.connect();
@@ -119,7 +105,10 @@ router.get('/properties/:id', async function(req, res, next) {
 			console.log(`Fetching file properties with sql:\n${sql}`);
 			const {rows} = await client.query(sql);
 
-			res.send(rows);
+			if(rows.length == 0)
+				res.sendStatus(404);
+			else
+				res.send(rows[0]);
 		} 
 		catch (err) {
 			console.error(err);
@@ -137,65 +126,44 @@ router.get('/properties/:id', async function(req, res, next) {
 // Create Requests
 //-----------------------------------------------------------------------------
 
-//TODO maybe have user send over a pregenned UID from their end, idk
-//TODO What if conflict with existing ID, especially deletedID? 
-
-//Create a new file
-//Returns the new file's UID
-router.put('/', function(req, res, next) {
-	const body = req.body;
-	console.log(`\nAttempting to create file`);
-
-	//Check that we have everything we need to create a new file
-	const requiredProps = ["accountuid", "filename", "parentuid", "isdirectory", "issymboliclink"]
-	const hasAllKeys = requiredProps.every(prop => Object.prototype.hasOwnProperty.call(body, prop));
-
-	//If we don't have all the required parameters...
-	if(!hasAllKeys) {
-		return res.status(422).send({
-			message: 'New file request must contain all of [accountuid, filename, parentuid, isdirectory, and issymboliclink]'
-		});
-	}
-
-	console.log(`Creating file with properties:`);
-	console.log(`accountuid='${body.accountuid}'`);
-	console.log(`filename='${body.filename}'`);
-	console.log(`parentuid='${body.parentuid}'`);
-	console.log(`isdirectory='${body.isdirectory}'`);
-	console.log(`issymboliclink='${body.issymboliclink}'`);
 
 
+//Put the file itself
+router.put('/:id', function(req, res, next) {
+	const fileUID = req.params.id;
+
+	//Steps:
+	//Check that fileUID exists
+	//Generate presigned URL
+	//Redirect request to presigned url for create/upload
 	(async () => {
-		const client = await POOL.connect();
-	
-		//TODO: Only returns ID when file is actually created, not on duplicate create requests.
-		//See https://stackoverflow.com/questions/34708509/how-to-use-returning-with-on-conflict-in-postgresql
+		updateStateTableDate(fileUID, "lastfileupdatedate", res);
+
 		try {
-			//Insert a new file with a random UID, using the parameters from 'body'. Return the UID.
-			const sql = "insert into file(fileuid, accountuid, filename, parentuid, isdirectory, issymboliclink, creationDate) "
-				+"values (gen_random_uuid (), '"+body.accountuid+"', '"+body.filename+"', "
-				+body.parentuid+", '"+body.isdirectory+"', '"+body.issymboliclink+"', (now() at time zone 'utc')) "
-				+"on conflict (accountuid, filename, parentuid) do nothing "
-				+"returning fileuid;";
-			console.log("Creating file with sql -");
-			console.log(sql);
-			
-			var newFileUID = await client.query(sql);	
-			console.log("EEEEEEEEE"+newFileUID);		
-			res.send(newFileUID);
+			//We don't care if the file exists in the database, there will be a cleanup job for orphan files
+
+			//Note: putObject creates a file if it doesn't already exist
+			console.log(`Generating signed put url...`);
+			IBMCOS.getSignedUrlPromise('putObject', { 
+				Bucket: IBMCOSBucket, 
+				Key: fileUID, 
+				Expires: 600 //seconds
+			})
+			.then(url => {
+				console.log(`Signed url generated: \n${url}`);
+				res.status(307).redirect(url);
+			});
 		} 
 		catch (e) {
-			console.error(`Error creating file in database: ${e.message}\n`);
+			console.error(`Error uploading file: ${e.message}\n`);
 			res.sendStatus(404);
-		} finally {
-			client.release();
 		}
 	})();
 });
 
 
-
-router.put('/:id', function(req, res, next) {
+//Put the file database entry
+router.put('/properties/:id', function(req, res, next) {
 	const fileUID = req.params.id;
 	const body = req.body;
 	console.log(`\nAttempting to write file with UID='${fileUID}'`);
@@ -246,7 +214,15 @@ router.put('/:id', function(req, res, next) {
 		return `'${prop}'`;
 	}).join(", ");
 
+
+	updateStateTableDate(fileUID, "lastdbupdatedate", res);
+
 	
+
+	//Note: If we don't trust the app to generate the FileUID, we'll need to use "returning fileuid" here.
+	//This only returns ID when file is actually created, not on duplicate create requests.
+	//See https://stackoverflow.com/questions/34708509/how-to-use-returning-with-on-conflict-in-postgresql
+
 	const sql = `insert into file (${props}, creationdate) `
 	+`values (${vals}, (now() at time zone 'utc')) `
 	+`on conflict (fileuid) do update `
@@ -255,23 +231,13 @@ router.put('/:id', function(req, res, next) {
 
 
 
-
-
 	(async () => {
 		const client = await POOL.connect();
-
-		//Note: If we don't trust the app to generate the FileUID, we'll need to use "returning fileuid" here.
-		// This only returns ID when file is actually created, not on duplicate create requests.
-		//See https://stackoverflow.com/questions/34708509/how-to-use-returning-with-on-conflict-in-postgresql
-
-		//TODO Null checks and omitting things if prop doesn't exist
 		try {
-
 			console.log("Putting file with sql -");
 			console.log(sql);
 			
-			var ret = await client.query(sql);	
-			console.log("NewShit EEEEEEEEEEEEEEE"+ret);		
+			var ret = await client.query(sql);
 			res.send(ret);
 		} 
 		catch (e) {
@@ -280,66 +246,19 @@ router.put('/:id', function(req, res, next) {
 		} finally {
 			client.release();
 		}
-
-
-
-		/*
-
-		//Steps:
-		//Check that fileUID exists
-		//Generate presigned URL
-		//Redirect request to presigned url for create/upload
-
-		try {
-			//Check that a file with this UID exists in the database
-			const sql = `SELECT EXISTS(SELECT 1 FROM file WHERE fileuid='${fileUID}' AND deleted != TRUE)`;
-			console.log(`Checking that file exists with sql:\n${sql}`);
-			var exists = await client.query(sql);
-			var exists = exists.rows[0].exists;
-
-			//If it doesn't exist, don't upload the file
-			if(!exists)
-				throw new Error(`FileUID not found! - ${fileUID}`);
-
-
-			//Note: putObject creates a file if it doesn't already exist
-			console.log(`Generating signed put url...`);
-			IBMCOS.getSignedUrlPromise('putObject', { 
-				Bucket: IBMCOSBucket, 
-				Key: fileUID, 
-				Expires: 600 //seconds
-			})
-			.then(url => {
-				console.log(`Signed url generated: \n${url}`);
-				res.status(307).redirect(url);
-			});
-		} 
-		catch (e) {
-			console.error(`Error uploading file: ${e.message}\n`);
-			res.sendStatus(404);
-		} finally {
-			client.release();
-		}
-		*/
 	})();
 });
 
 
 
-router.delete('/delete/:id', function(req, res, next) {
+
+//Delete the file itself
+router.delete('/:id', function(req, res, next) {
 	const fileUID = req.params.id;
 	console.log(`\nAttempting to delete file with UID='${fileUID}'`);
 
-
 	(async () => {
-	  const client = await POOL.connect();
 		try {
-			//Get the file data's location
-			const sql = `UPDATE file SET deleted = true WHERE fileuid='${fileUID}'`;
-			console.log(`Setting file deleted with sql:\n${sql}`);
-			await client.query(sql);			
-
-
 			//Delete object from IBM COS (sets delete marker)
 			console.log(`Deleting object from IBM COS...`);
 			IBMCOS.deleteObject({
@@ -355,6 +274,32 @@ router.delete('/delete/:id', function(req, res, next) {
 			console.error(`Error deleting file: ${e.code} - ${e.message}\n`);
 			res.send(e);
 			//res.sendStatus(404);
+		}
+	})();
+});
+
+
+//Delete the file from the database (sets delete marker)
+router.delete('/properties/:id', function(req, res, next) {
+	const fileUID = req.params.id;
+	console.log(`\nAttempting to delete file properties with UID='${fileUID}'`);
+
+	(async () => {
+	  const client = await POOL.connect();
+		try {
+			//Get the file data's location
+			const sql = `UPDATE file SET deleted = true WHERE fileuid='${fileUID}'`;
+			console.log(`Setting file deleted with sql:\n${sql}`);
+			await client.query(sql);
+
+
+			//Always send a 200 unless delete actually failed, don't want to give out info
+			res.sendStatus(200);
+		} 
+		catch (e) {
+			console.error(`Error deleting file: ${e.code} - ${e.message}\n`);
+			res.send(e);
+			//res.sendStatus(404);
 		} finally {
 			client.release();
 		}
@@ -364,3 +309,54 @@ router.delete('/delete/:id', function(req, res, next) {
 
 
 module.exports = router;
+
+
+
+
+
+/*
+//TODO Maybe require a body param with accountid or something? Sending only files for that account?
+router.get('/', function(req, res, next) {
+	console.log(`\nAttempting to fetch all files`);
+
+	const query = req.query;
+	console.log(`Available queries:`);
+	console.log(query);
+
+	//Grab any conditions we care about from the parameters sent with the request 
+	var conditions = [];
+	if(query.accountuid !== undefined) conditions.push("accountuid = '"+query.accountuid+"'");
+	if(query.parentuid !== undefined) conditions.push("parentuid = '"+query.parentuid+"'");
+	if(query.fileuid !== undefined) conditions.push("fileuid = '"+query.fileuid+"'");
+	console.log(`Accepted conditions:`);
+	console.log(conditions);
+
+
+	//Combine the conditions into a usable where query
+	const where = conditions.length > 0 ? " WHERE "+conditions.join(" AND ") : "";
+
+
+	var getFileSql  = "SELECT fileuid, filename, isdirectory, issymboliclink, "
+			+"accountuid, parentuid, creationdate, deleted FROM file";
+	getFileSql += where;
+	getFileSql += " LIMIT 500;";
+	
+
+	(async () => {
+	  const client = await POOL.connect();
+
+		try {
+			console.log(`Selecting files with sql:\n${getFileSql}`);
+			const {rows} = await client.query(getFileSql);
+
+			res.send(rows);
+		} 
+		catch (err) {
+			console.error(err);
+			res.send(err);
+		} finally {
+			client.release();
+		}
+	})();
+});
+*/
