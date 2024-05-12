09130c6a8b9cc2f024cb7efd106dd5367ecc1800
Sgordon4 <SeanGordonkh@gmail.com>
2024-05-12 14:40:28 -0500

    Prob final iteration

--- routes/files.js
+++ routes/files.js
@@ -6,6 +6,250 @@ const {POOL} = require('#root/database/postgresPool.js');
 const {IBMCOS, IBMCOSBucket} = require('#root/storage/IBMCOS');
 
 
+/*
+Notes: 
+
+Do we want to exlude deleted files?
+Thinking about trashed files, we could simply put a trashedtime in the ordering and that would suffice,
+Wait yeah that's it. If two dirs ref one fileuid, and one trashes it, we can't just delete it. 
+*/
+
+
+
+
+//Get the file properties
+router.get('/:id', async function(req, res, next) {
+	const fileUID = req.params.id;
+	console.log(`\nGET FILE called with fileUID='${fileUID}'`);
+
+
+	var sql =
+	`SELECT fileuid, owneruid, isdir, islink, filesize, fileblocks, 
+	changetime, accesstime, modifytime, createtime FROM file
+	WHERE fileuid = '${fileUID}'
+	AND deletetime is null;`;
+
+	(async () => {
+		const client = await POOL.connect();
+		try {
+			console.log(`Fetching file properties with sql -`);
+			console.log(sql.replaceAll("\t","").replaceAll("\n", " "));
+			const {rows} = await client.query(sql);
+
+
+			if(rows.length == 0)
+				res.sendStatus(404);
+			else
+				res.send(rows[0]);
+		} 
+		catch (err) {
+			console.error(err);
+			res.send(err);
+		}
+		finally { client.release(); }
+	})();
+});
+
+//Create a new file
+router.post('/', async function(req, res, next) {
+	console.log(`\nCREATE FILE called`);
+
+	const body = req.body;
+	if(!body.owneruid) {
+		console.log(`File create request must contain owneruid!`);
+		return res.status(422).send({ message: `File create request must contain owneruid!` });
+	}
+
+
+	//Grab the properties we care about
+	const usefulProps = ["owneruid", "isdir", "islink"];
+
+	var propHelper = [];
+	var valueHelper = [];
+	for(const [key, val] of Object.entries(body)) {
+		if(val && usefulProps.includes(key)) {
+			propHelper.push(key);
+			valueHelper.push(`'${val}'`);
+		}
+	}
+
+	
+	const sql = 
+	`WITH fileupdate AS
+	(
+		INSERT INTO file (${propHelper.join(", ")})
+		VALUES (${valueHelper.join(", ")})
+		RETURNING *
+	),
+	journalupdate AS (
+		INSERT INTO journal
+		(fileuid, owneruid, filesize, fileblocks)
+		SELECT fileuid, owneruid, filesize, fileblocks
+		FROM fileupdate
+	)
+	SELECT fileuid, owneruid, isdir, islink, filesize, fileblocks, createtime FROM fileupdate;`;
+
+	(async () => {
+		const client = await POOL.connect();
+		try {
+			console.log("Creating new file with sql -");
+			console.log(sql.replaceAll("\t","").replaceAll("\n", " "));
+			
+			var ret = await client.query(sql);
+			res.send(ret.rows[0]);
+		} 
+		catch (err) {
+			console.error(err);
+			res.send(err);
+		}
+		finally { client.release(); }
+	})();
+});
+
+//Update a file
+router.put('/:id', async function(req, res, next) {
+	res.send("Stub");
+});
+
+//Delete a file (sets delete time, job picks it up later)
+router.delete('/:id', async function(req, res, next) {
+	const fileUID = req.params.id;
+	console.log(`\nDELETE FILE called with fileUID='${fileUID}'`);
+
+
+	const sql = 
+	`WITH fileupdate AS
+	(
+		UPDATE file
+		SET deletetime = (now() at time zone 'utc')
+		WHERE fileuid = '${fileUID}'
+		RETURNING *
+	),
+	journalupdate AS (
+		INSERT INTO journal
+		(fileuid, owneruid, filesize, fileblocks)
+		SELECT fileuid, owneruid, filesize, fileblocks
+		FROM fileupdate
+	)
+	SELECT fileuid, owneruid, isdir, islink, filesize, fileblocks, createtime FROM fileupdate;`;
+
+	(async () => {
+		const client = await POOL.connect();
+		try {
+			console.log("Deleting file with sql -");
+			console.log(sql.replaceAll("\t","").replaceAll("\n", " "));
+			
+			await client.query(sql);			
+			res.sendStatus(200);
+		} 
+		catch (err) {
+			console.error(`Error deleting file: ${err.code} - ${err.message}\n`);
+			res.send(err);
+			//res.sendStatus(404);
+		}
+		finally { client.release(); }
+	})();
+});
+
+
+//-----------------------------------------------------------------------------
+
+
+//TODO THIS DOESN'T WORK, we need to compile blocks on the client side or whatever
+//Get the file url
+router.get('/url/:id', async function(req, res, next) {
+	const fileUID = req.params.id;
+	console.log(`\nGET FILE URL called with fileuid='${fileUID}'`);
+
+
+	console.log(`Generating signed get url...`);
+	IBMCOS.getSignedUrlPromise('getObject', { 
+		Bucket: IBMCOSBucket, 
+		Key: fileUID, 
+		//Key: "smiley.png", 
+		Expires: 120 //seconds	//Prob want this longer, maybe just use default 15min
+	})
+	.then(url => {
+		console.log(`Signed url generated: \n${url}`);
+		res.redirect(url);
+	});
+});
+
+//Update a blockset
+router.put('/commit/:hash', async function(req, res, next) {
+	const blockHash = req.params.hash;
+	console.log(`\nPUT BLOCK called with hash='${blockHash}'`);
+
+	//Check that all blocks exist
+
+
+	(async () => {
+		const client = await POOL.connect();
+		try {
+			console.log(`Fetching file properties with sql -`);
+			console.log(sql.replaceAll("\t","").replaceAll("\n", " "));
+			const {rows} = await client.query(sql);
+
+
+			if(rows.length == 0)
+				res.sendStatus(404);
+			else
+				res.send(rows[0]);
+		} 
+		catch (err) {
+			console.error(err);
+			res.send(err);
+		}
+		finally { client.release(); }
+	})();
+
+
+
+	
+});
+
+
+
+/*
+
+
+	//If the block already exists, don't allow reupload
+	var sql = 
+	`SELECT EXISTS (
+		SELECT 1 FROM file 
+		WHERE fileuid = '5b600697-2065-4266-ac9d-43deb8e9ba20'
+	);`;
+
+	//Get a presigned PUT url to upload to the block
+	console.log(`Generating signed put url...`);
+	IBMCOS.getSignedUrlPromise('putObject', { 
+		Bucket: IBMCOSBucket, 
+		Key: fileUID, 
+		Expires: 60 //seconds
+	})
+	.then(url => {
+		console.log(`Signed url generated: \n${url}`);
+		res.send(url);
+	});
+*/
+
+
+
+
+
+
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+
+
+
+
+
+
 
 //Get the file properties for the provided ID
 router.get('/properties/:id', async function(req, res, next) {
