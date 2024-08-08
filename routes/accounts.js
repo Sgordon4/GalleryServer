bb628ad2c59391013fbb49d8a15439ab14d17bd4
Sgordon4 <SeanGordonkh@gmail.com>
2024-08-08 06:35:53 -0500

    Finished fixing account routing

--- routes/accounts.js
+++ routes/accounts.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const {POOL} = require("../database/postgresPool");
 
 
+//TODO Better error handling
 
 
 router.get('/:id', async function(req, res, next) {
@@ -66,12 +67,16 @@ router.put('/' , async function(req, res, next) {
 
 
 	//Make sure we have what we need to create the account
-	reqInsert.forEach(column => {
+	for(var i = 0; i < reqInsert.length; i++) {
+		var column = reqInsert[i];
 		if(props.indexOf(column) == -1) {
-			console.log(`Account create request must contain ${column}!`);
-			return res.status(422).end();		//Is there a code we should send
+			console.log(`Account creation failed!`);
+			var errJson = `{"status" : "fail", `
+				+`"data" : {"${column}" : "Account create request must contain ${column}!"}}`
+			console.log(errJson);
+			return res.status(422).send(errJson);
 		}
-	});
+	};
 
 
 
@@ -86,46 +91,43 @@ router.put('/' , async function(req, res, next) {
 			console.log("Inserting account with sql -");
 			console.log(sql.replaceAll("\t","").replaceAll("\n", " "));
 			
-			var ret = await client.query(sql);
-			
+			var ret = await client.query(sql);	
 			res.send(ret.rows[0]);
 		} 
 		catch (err) {
-			
+			console.log(`Account creation failed!`);
 
-			console.log(err);
-			res.send(err);
-
-			/*
 			//If this email already exists, inform the client
 			if(err.code == '23505') {
-				console.log(`Account creation failed!`);
-				console.log(`An account already exists with email='${body.email}!'`);
-				res.status(409).send({message: `An account already exists with email='${body.email}'!`});
+				var errJson = `{"status" : "fail", `
+				+`"data" : {"email" : "An account already exists with email='${body.email}'!"}}`
+
+				console.log(errJson);
+				res.status(409).send(errJson);
 			}
 			else {
 				console.log(err);
-				res.send(err);
+				res.status(409).send(err);
 			}
-			*/
 		}
 		finally { client.release(); }
 	})();
 });
 
 
+//-----------------------------------------------------------------------------
 
 
-//Upsert an account
-router.put('/' , async function(req, res, next) {
-	console.log(`\nUPSERT ACCOUNT called`);
+router.put('/:id' , async function(req, res, next) {
+	console.log(`\nUPDATE ACCOUNT called`);
+	const accountUID = req.params.id;
 	const body = req.body;
 
-	
+
+	//Accounts can be created on a local device, and then copied to the server later.
+	//We need to allow all columns to be sent to allow for that. 
 	const allProps = ["accountuid", "rootfileuid", "email", "displayname", "password", 
 		"isdeleted", "logintime", "changetime", "createtime"];
-	const reqInsert = ["accountuid", "rootfileuid", "email", "displayname", "password"];
-	const reqUpdate = ["accountuid"];
 
 	//Grab any valid properties passed in the response body
 	var props = [];
@@ -138,41 +140,43 @@ router.put('/' , async function(req, res, next) {
 	}
 
 
+	//Make sure we have at least 1 column for the update
+	if(props.length < 1) {
+		console.log(`Account update failed!`);
+		var errJson = `{"status" : "fail", "data" : null, "message" : `+
+		`"Account update requires at least one of the following columns: [${allProps.join(", ")}]"}`
+		console.log(errJson);
+		return res.status(422).send(errJson);
+	}
+	for(var i = 0; i < reqInsert.length; i++) {
+		var column = reqInsert[i];
+		if(props.indexOf(column) == -1) {
+			
+		}
+	};
 
-	const sql = `INSERT INTO account (${props.join(", ")}) VALUES (${vals.join(", ")})
-			ON CONFLICT (accountuid) DO NOTHING
+
+	//Can't use parentheses with only one column
+	const pr = (props.length == 1) ? `${props[0]}` : `(${props.join(", ")})`;
+	const sql = `UPDATE account SET ${pr} = (${vals.join(", ")})
+			WHERE accountuid = '${accountUID}'
 			RETURNING *;`;
 
-	
 
 	(async () => {
 		const client = await POOL.connect();
 		try {
-			console.log("Upserting account with sql -");
+			console.log("Updating account with sql -");
 			console.log(sql.replaceAll("\t","").replaceAll("\n", " "));
 			
-			var ret = await client.query(sql);
-			
+			var ret = await client.query(sql);	
 			res.send(ret.rows[0]);
 		} 
 		catch (err) {
-			
-
+			console.log(`Account update failed!`);
+		
 			console.log(err);
-			res.send(err);
-
-			/*
-			//If this email already exists, inform the client
-			if(err.code == '23505') {
-				console.log(`Account creation failed!`);
-				console.log(`An account already exists with email='${body.email}!'`);
-				res.status(409).send({message: `An account already exists with email='${body.email}'!`});
-			}
-			else {
-				console.log(err);
-				res.send(err);
-			}
-			*/
+			res.status(409).send(err);
 		}
 		finally { client.release(); }
 	})();
