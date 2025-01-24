288a2b3a96d71cec3492dae63975779a9376ee50
SGordon4 <seangordonkh@gmail.com>
2025-01-24 01:14:46 -0600

    Completed and tested file put/delete endpoints

--- routes/files.js
+++ routes/files.js
@@ -1,7 +1,7 @@
 var express = require('express');
 const { ExpressValidator } = require('express-validator');
-const { check, matchedData, validationResult } = require('express-validator');
-const { body } = new ExpressValidator({}, {
+const { header, matchedData, validationResult } = require('express-validator');
+const { body, param } = new ExpressValidator({}, {
 	wrap: value => {
 	  return "'"+value+"'";
 	},
@@ -67,6 +67,8 @@ router.get('/:id', async function(req, res, next) {
 const fileTableColumns = ["fileuid", "accountuid", "isdir", "islink", "checksum", "filesize", 
 	"userattr", "attrhash", "changetime", "modifytime", "accesstime", "createtime"]
 
+const fileUIDParamCheck = () => param('fileuid').isUUID().withMessage("Must be a UUID!").wrap();
+
 const fileUIDCheck = () => body('fileuid').isUUID().withMessage("Must be a UUID!").wrap();
 const accountUIDCheck = () => body('accountuid').isUUID().withMessage("Must be a UUID!").wrap();
 const isDirCheck = () => body('isdir').isBoolean().withMessage("Must be a boolean!");
@@ -81,8 +83,10 @@ const accesstimeCheck = () => body('accesstime').isInt().withMessage("Must be an
 const createtimeCheck = () => body('createtime').isInt().withMessage("Must be an epoch value!");
 
 const deviceUIDCheck = () => body('deviceuid').isUUID().withMessage("Must be a UUID!");
-const ifMatchCheck = () => check("If-Match").isHash('sha256').withMessage("Must be an SHA256 hash!");
+const ifMatchCheck = () => header("If-Match").isHash('sha256').withMessage("Must be an SHA256 hash!");
 
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 const createValidations = [fileUIDCheck(), accountUIDCheck(), deviceUIDCheck(),
@@ -140,8 +144,8 @@ router.put('/create', createValidations, async function(req, res, next) {
 				attrhash: fileProps.attrhash,
 				changetime: fileProps.changetime,
 				createtime: fileProps.createtime
-			}
-			putJournal(client, fileProps.fileuid, fileProps.accountuid, deviceUID, JSON.stringify(changes), fileProps.changetime)
+			};
+			putJournal(client, fileProps.fileuid, fileProps.accountuid, deviceUID, JSON.stringify(changes), fileProps.changetime);
 
 
 			return res.status(201).send(fileProps);
@@ -157,11 +161,9 @@ router.put('/create', createValidations, async function(req, res, next) {
 
 
 
-
+//----------------------------------------------------------------------------------------------------------------------------------------------------
 
 //https://developer.mozilla.org/en-US/docs/Web/HTTP/Conditional_requests#avoiding_the_lost_update_problem_with_optimistic_locking
-
-//TODO Is there a way to return different things from the db depending on how the where fails, e.g. hash fail vs does not exist? 
 
 const contentValidations = [fileUIDCheck(), accountUIDCheck(), deviceUIDCheck(), ifMatchCheck(),
 	checksumCheck(), fileSizeCheck(), changetimeCheck().optional(), modifytimeCheck().optional()]
@@ -178,48 +180,57 @@ router.put('/content', contentValidations, async function(req, res, next) {
 	//If-Match is used to avoid lost updates
 	const ifMatch = data["if-match"];
 	delete data["if-match"]; 
-	console.log("If-Match: "+ifMatch);
 
-	//deviceUID is needed for use in Journal, but is not actually a field in the file database
+	//These items are needed for where clauses and adding to Journal, but do not need to be updated in the file table
+	const fileUID = data.fileuid;
+	delete data.fileuid;
+
+	const accountUID = data.accountuid;
+	delete data.accountuid;
+
 	const deviceUID = data.deviceuid;
-	delete data.deviceuid; 
-
+	delete data.deviceuid;
 
 	//If changetime or modifytime weren't included, set them to the current epoch timestamp
 	data.changetime = data.changetime || "extract(epoch from date_trunc('second', (now() at time zone 'utc')))";
 	data.modifytime = data.modifytime || "extract(epoch from date_trunc('second', (now() at time zone 'utc')))";
 
 
-	const keys = Object.keys(data);
-	const vals = Object.values(data);
+	var fileExistsSql = `SELECT fileuid FROM file WHERE fileuid = ${fileUID} and isdeleted = false;`
 
-	var sql = `UPDATE file SET (${keys.join(", ")}) = (${vals.join(", ")}) `;
-	sql += `WHERE file.fileuid = ${data.fileuid} AND file.checksum = '${ifMatch}'AND file.isdeleted IS false `;
+	const joined = Object.entries(data).map(item => item.join(" = ")).join(", ");
+	var sql = `UPDATE file SET ${joined} `;
+	sql += `WHERE file.fileuid = ${fileUID} AND file.checksum = '${ifMatch}' AND file.isdeleted IS false `;
 	sql += `RETURNING ${fileTableColumns.join(", ")};`;
+
 
 	(async () => {
 		const client = await POOL.connect();
 		try {
 			console.log("Updating file contents with sql -");
 			console.log(sql.replaceAll("\t","").replaceAll("\n", " "));
-			
-			var ret = await client.query(sql);
 
-			//If we don't get anything back, that means the update failed and the file does not exist
-			if(ret.rows.length == 0)
+			//If we don't get anything back, that means the file does not exist
+			var exists = await client.query(fileExistsSql);
+			if(exists.rows.length == 0)
 				return res.status(404).send("File does not exist!");
 
-			const fileProps = ret.rows[0];
+
+			//If we don't get anything back, that means the checksum doesn't match.
+			//Technically the file could have also *just* been deleted by another process.
+			var ret = await client.query(sql);
+			if(ret.rows.length == 0)
+				return res.status(412).send("File checksums don't match!");
 
 
 			//Add an entry to the Journal with the new file information
+			const fileProps = ret.rows[0];
 			const changes = {
 				checksum: fileProps.checksum,
-				attrhash: fileProps.attrhash,
 				changetime: fileProps.changetime,
 				modifytime: fileProps.modifytime
-			}
-			putJournal(client, fileProps.fileuid, fileProps.accountuid, deviceUID, JSON.stringify(changes), fileProps.changetime)
+			};
+			putJournal(client, fileProps.fileuid, fileProps.accountuid, deviceUID, JSON.stringify(changes), fileProps.changetime);
 
 
 			return res.status(200).send(fileProps);
@@ -234,28 +245,223 @@ router.put('/content', contentValidations, async function(req, res, next) {
 });
 	
 
-router.put('/attrs', async function(req, res, next) {
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+const attributeValidations = [fileUIDCheck(), accountUIDCheck(), deviceUIDCheck(), ifMatchCheck(), userAttrCheck(), changetimeCheck().optional()]
+
+router.put('/attrs', attributeValidations, async function(req, res, next) {
 	console.log(`\nUPDATE FILE ATTRS called`);
-	const body = req.body;
+	if(!validationResult(req).isEmpty()) {
+		console.log("Body data has issues, cannot update attributes!");
+		return res.status(422).send({ errors: validationResult(req).array() });
+	}
+
+	const data = matchedData(req);
+
+	//If-Match is used to avoid lost updates
+	const ifMatch = data["if-match"];
+	delete data["if-match"]; 
+
+	//These items are needed for where clauses and adding to Journal, but do not need to be updated in the file table
+	const fileUID = data.fileuid;
+	delete data.fileuid;
+
+	const accountUID = data.accountuid;
+	delete data.accountuid;
+
+	const deviceUID = data.deviceuid;
+	delete data.deviceuid;
+
+	//If changetime wasn't included, set it to the current epoch timestamp
+	data.changetime = data.changetime || "extract(epoch from date_trunc('second', (now() at time zone 'utc')))";
+
+
+	var fileExistsSql = `SELECT fileuid FROM file WHERE fileuid = ${fileUID} and isdeleted = false;`
+
+	const joined = Object.entries(data).map(item => item.join(" = ")).join(", ");
+	var sql = `UPDATE file SET ${joined} `;
+	sql += `WHERE file.fileuid = ${fileUID} AND file.attrhash = '${ifMatch}' AND file.isdeleted IS false `;
+	sql += `RETURNING ${fileTableColumns.join(", ")};`;
+
+
+	(async () => {
+		const client = await POOL.connect();
+		try {
+			console.log("Updating file attributes with sql -");
+			console.log(sql.replaceAll("\t","").replaceAll("\n", " "));
+
+			//If we don't get anything back, that means the file does not exist
+			var exists = await client.query(fileExistsSql);
+			if(exists.rows.length == 0)
+				return res.status(404).send("File does not exist!");
+
+
+			//If we don't get anything back, that means the checksum doesn't match.
+			//Technically the file could have also *just* been deleted by another process.
+			var ret = await client.query(sql);
+			if(ret.rows.length == 0)
+				return res.status(412).send("File attrhash doesn't match!");
+
+
+			//Add an entry to the Journal with the new file information
+			const fileProps = ret.rows[0];
+			const changes = {
+				attrhash: fileProps.attrhash,
+				changetime: fileProps.changetime,
+			};
+			putJournal(client, fileProps.fileuid, fileProps.accountuid, deviceUID, JSON.stringify(changes), fileProps.changetime);
+
+
+			return res.status(200).send(fileProps);
+		} 
+		catch (err) {
+			console.log(`File attribute update failed!`);
+			console.log(err);
+			return res.status(500).send(err);
+		}
+		finally { client.release(); }
+	})();
 });
 
-router.put('/timestamps', async function(req, res, next) {
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+const timestampValidations = [fileUIDCheck(), accountUIDCheck(), deviceUIDCheck(),
+	changetimeCheck().optional(), modifytimeCheck().optional(), accesstimeCheck().optional()]
+
+router.put('/timestamps', timestampValidations, async function(req, res, next) {
 	console.log(`\nUPDATE FILE TIMESTAMPS called`);
-	const body = req.body;
+	if(!validationResult(req).isEmpty()) {
+		console.log("Body data has issues, cannot update timestamps!");
+		return res.status(422).send({ errors: validationResult(req).array() });
+	}
+
+	const data = matchedData(req);
+
+	//These items are needed for where clauses and adding to Journal, but do not need to be updated in the file table
+	const fileUID = data.fileuid;
+	delete data.fileuid;
+
+	const accountUID = data.accountuid;
+	delete data.accountuid;
+
+	const deviceUID = data.deviceuid;
+	delete data.deviceuid;
+	
+	//If no timestamp was included, we have nothing to update
+	if(Object.keys(data).length == 0) 
+		return res.status(422).send("File timestamp update must contain at least one of [changetime, modifytime, accesstime]!");
+
+	//If changetime wasn't included, set it to the current epoch timestamp
+	data.changetime = data.changetime || "extract(epoch from date_trunc('second', (now() at time zone 'utc')))";
+		
+
+
+
+	const joined = Object.entries(data).map(item => item.join(" = ")).join(", ");
+	var sql = `UPDATE file SET ${joined} `;
+	sql += `WHERE file.fileuid = ${fileUID} AND file.isdeleted IS false `;
+	sql += `RETURNING ${fileTableColumns.join(", ")};`;
+
+
+	(async () => {
+		const client = await POOL.connect();
+		try {
+			console.log("Updating file timestamps with sql -");
+			console.log(sql.replaceAll("\t","").replaceAll("\n", " "));
+
+			//If we don't get anything back, that means the file does not exist
+			var ret = await client.query(sql);
+			if(ret.rows.length == 0)
+				return res.status(404).send("File does not exist!");
+
+
+			//Add an entry to the Journal with the new file information
+			const fileProps = ret.rows[0];
+			const changes = {};
+			for(const key of Object.keys(data)) {
+				changes[key] = fileProps[key];
+			};
+			putJournal(client, fileProps.fileuid, fileProps.accountuid, deviceUID, JSON.stringify(changes), fileProps.changetime);
+
+
+			return res.status(200).send(fileProps);
+		} 
+		catch (err) {
+			console.log(`File timestamp update failed!`);
+			console.log(err);
+			return res.status(500).send(err);
+		}
+		finally { client.release(); }
+	})();
 });
 
 
-router.put('/delete', async function(req, res, next) {
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+const deleteValidations = [fileUIDCheck(), accountUIDCheck(), deviceUIDCheck()]
+
+router.delete('/', deleteValidations, async function(req, res, next) {
 	console.log(`\nDELETE FILE called`);
-	const body = req.body;
+	if(!validationResult(req).isEmpty()) {
+		console.log("Body data has issues, cannot update timestamps!");
+		return res.status(422).send({ errors: validationResult(req).array() });
+	}
+
+	const data = matchedData(req);
+
+
+	//These items are needed for where clauses and adding to Journal, but do not need to be updated in the file table
+	const fileUID = data.fileuid;
+	delete data.fileuid;
+
+	const accountUID = data.accountuid;
+	delete data.accountuid;
+
+	const deviceUID = data.deviceuid;
+	delete data.deviceuid;
+	
+
+	var sql = `UPDATE file SET isdeleted = true `;
+	sql += `WHERE file.fileuid = ${fileUID} AND file.isdeleted IS false `;
+	sql += `RETURNING ${fileTableColumns.join(", ")};`;
+
+
+	(async () => {
+		const client = await POOL.connect();
+		try {
+			console.log("Deleting file with sql -");
+			console.log(sql.replaceAll("\t","").replaceAll("\n", " "));
+
+			//If we don't get anything back, that means the file does not exist or is already deleted
+			var ret = await client.query(sql);
+			if(ret.rows.length == 0)
+				return res.status(404).send("File does not exist!");
+
+
+			//Add an entry to the Journal with the new file information
+			const fileProps = ret.rows[0];
+			const changes = { isdeleted: true };
+			putJournal(client, fileProps.fileuid, fileProps.accountuid, deviceUID, JSON.stringify(changes), fileProps.changetime);
+
+
+			return res.status(200).send(fileProps);
+		} 
+		catch (err) {
+			console.log(`File delete failed!`);
+			console.log(err);
+			return res.status(500).send(err);
+		}
+		finally { client.release(); }
+	})();
 });
 
 
 
-
-
-
-
+//----------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
@@ -273,182 +479,6 @@ async function putJournal(client, fileUID, accountUID, deviceUID, changes, chang
 	if(ret.rows.length == 0)
 		throw new Error("Journal insert failed!");
 }
-
-
-
-
-
-//-----------------------------------------------------------------------------
-
-
-//Upsert file
-router.put('/', async function(req, res, next) {
-	console.log(`\nUPSERT FILE called`);
-	const body = req.body;
-	
-
-	//Files can be created on a local device, and then copied to the server later.
-	//We need to allow all columns to be sent to allow for that. 
-	const allProps = ["fileuid", "accountuid", "isdir", "islink", "filesize", "filehash", 
-		"userattr", "attrhash", "changetime", "modifytime", "accesstime", "createtime"]
-	const reqInsert = ["fileuid", "accountuid"];
-
-
-	//Make sure we have what we need to create a file
-	for(const column of reqInsert) {
-		if(body[column] == undefined) {
-		//if(passedEntries.indexOf(column) == -1) {
-			console.log(`File creation failed!`);
-			var errJson = `{"status" : "fail", "data" : {"${column}" : "File upsert request must contain ${column}!"}}`
-			console.log(errJson);
-			return res.status(422).send(errJson);
-		}
-	}
-
-
-
-	//Grab any valid properties passed in the response body
-	var props = [];
-	var vals = [];
-	
-	//We want every property included, even if they were not passed.
-	for(const key of allProps) {
-		var val = body[key];
-
-		//If a property was not passed, set it to its default. 
-		//WARNING: This could backfire if this isn't considered when sending properties. Will be fine for our purposes.
-		if(val == undefined)
-			val = "DEFAULT";
-
-		props.push(key);
-
-		//Postgres array notation is ass
-		if(key == "userattr" && val == "{")
-			vals.push(`'${val}'`);
-		else
-			vals.push(`${val}`);
-	}
-	
-
-
-	var sql = `INSERT INTO file (${props.join(", ")}) VALUES (${vals.join(", ")}) `;
-
-
-	
-	//Edit values for the UPDATE part of the sql ----------------------------
-	
-	//Remove fileuid from properties as it should not be updated (fileuid is guaranteed to be the first element)
-	props.shift();
-	vals.shift();
-
-	//If changetime is not manually specified, we want to set it for the UPDATE part of the query
-	if(props.indexOf("changetime") == -1) {
-		props.push("changetime");
-		vals.push("extract(epoch from date_trunc('second', (now() at time zone 'utc')))");
-	}
-
-	//Ensure the file is not hidden. If we move a file from s->l, the server file is 'deleted' by hiding it.
-	//However, if we go back from l->s, the file will still be hidden without this change below.
-	//Doing this via trigger on update has proven unsuccessful (possible, but touchy)
-	props.push("isdeleted");
-	vals.push("false");
-
-
-
-	sql += `ON CONFLICT (fileuid) DO UPDATE SET (${props.join(", ")}) = (${vals.join(", ")}) `;
-
-
-
-	//Compare filehash if one was included
-	var fileHashWhere;
-	if(req.query.prevfilehash == undefined)
-		fileHashWhere = `(file.filehash IS NULL OR file.isdeleted IS true) `
-	else if(req.query.prevfilehash != null) 
-		fileHashWhere = `file.filehash = '${req.query.prevfilehash}' `
-
-	//Compare attrhash if one was included
-	var attrHashWhere;
-	if(req.query.prevattrhash == undefined)
-		attrHashWhere = `(file.attrhash IS NULL OR file.isdeleted IS true) `
-	else if(req.query.prevattrhash != null) 
-		attrHashWhere = `file.attrhash = '${req.query.prevattrhash}' `
-
-	//Join them together into one WHERE statement as long as they aren't null
-	if(fileHashWhere != null || attrHashWhere != null)
-		sql += "WHERE " + [fileHashWhere, attrHashWhere].filter(Boolean).join("AND ");
-
-
-	//NOTE: attrhash must be returned by this 'RETURNING ...' section or the journal trigger 
-	// will not work, as it won't find a changed attrhash in the returned props
-
-	sql += `RETURNING ${allProps.join(", ")};`;
-
-	//Replace all double quotes with single, postgres doesn't like that.
-	//Note: We don't have any columns that use quotes internally atm, but this would cause problems for ones that do.
-	//sql = sql.replace(/"/g, "'");
-	sql = sql.replace(/""/g, "''");
-
-
-	(async () => {
-		const client = await POOL.connect();
-		try {
-			console.log("Upserting file with sql -");
-			console.log(sql.replaceAll("\t","").replaceAll("\n", " "));
-			
-			var ret = await client.query(sql);
-
-			//If we don't get anything back, that means the insert failed (99% chance prevHashes don't match)
-			if(ret.rows.length == 0)
-				res.status(412).send("Hashes do not match");
-
-			res.send(ret.rows[0]);
-		} 
-		catch (err) {
-			console.log(`File upsert failed!`);
-
-			console.log(err);
-			res.status(409).send(err);
-		}
-		finally { client.release(); }
-	})();
-
-});
-
-
-//-----------------------------------------------------------------------------
-
-
-//'Delete' the file by setting isdeleted=true
-router.delete('/:id', function(req, res, next) {
-	const fileUID = req.params.id;
-	console.log(`\nDELETE FILE called with fileuid='${fileUID}'`);
-
-
-	const sql = 
-	`UPDATE file SET isdeleted = true
-	WHERE fileuid = '${fileUID}'
-	RETURNING *`;
-
-	(async () => {
-		const client = await POOL.connect();
-		try {
-			console.log("Deleting file entry with sql -");
-			console.log(sql.replaceAll("\t","").replaceAll("\n", " "));
-			const {rows} = await client.query(sql);
-
-
-			if(rows.length == 0)
-				res.sendStatus(404);
-			else
-				res.sendStatus(200);
-		} 
-		catch (err) {
-			console.error(err);
-			res.send(err);
-		}
-		finally { client.release(); }
-	})();
-});
 
 
 
